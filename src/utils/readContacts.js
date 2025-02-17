736a3260e22bd529a1ee94e86705a3b2d4bd6f2b
olexiy-kireyko <a.kireyko@gmail.com>
2025-02-17 19:24:06 +0200

    hw

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const content = await fs.readFile(PATH_DB, 'utf8');
+    return JSON.parse(content);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,10 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    fs.writeFile(PATH_DB, JSON.stringify(updatedContacts), 'utf-8');
+  } catch (error) {
+    console.log(error);
+  }
+};
